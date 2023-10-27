1486d1d76139a2e6374e7f09a05eda40502aacbf
mohitpraja <immohitpraja@gmail.com>
2023-10-27 22:38:55 +0530

    dating view done

--- lib/datingApp/controller/dating_conroller.dart
+++ lib/datingApp/controller/dating_conroller.dart
@@ -5,13 +5,30 @@ import 'package:get/get.dart';
 
 class DatingController extends GetxController {
   RxList<DatingModel> personList = <DatingModel>[].obs;
+  RxList<DatingModel> tempList = <DatingModel>[].obs;
 
   @override
   Future<void> onInit() async {
     var datingJson=await rootBundle.loadString("assets/json/dating_data.json");
     personList.value= datingDataResponseFromJson(datingJson);
+    tempList.value= datingDataResponseFromJson(datingJson);
     super.onInit();
 
   }
+
+  runFilter(name){
+    List<DatingModel> result=tempList;
+    if(name.isEmpty||name==''){
+      personList.value=tempList;
+    }else{
+      result=tempList.where((p0) => p0.name!.toLowerCase().toUpperCase().contains(name.toLowerCase().toUpperCase())).toList();
+    }
+    personList.value=result;
+  }
+
+
+
+
+
   List<DatingModel> datingDataResponseFromJson(String str) => List<DatingModel>.from(json.decode(str).map((x) => DatingModel.fromJson(x)));
 }
--- lib/datingApp/view/dating_view.dart
+++ lib/datingApp/view/dating_view.dart
@@ -8,39 +8,180 @@ class DatingView extends GetView<DatingController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: const [
-          Padding(
-            padding: EdgeInsets.only(right: 10),
-            child: Text(
-              'SELECT',
-              style: TextStyle(
-                  letterSpacing: 1.5,
-                  fontSize: 13,
-                  color: Colors.deepOrange,
-                  fontWeight: FontWeight.bold),
-            ),
-          )
-        ],
-      ),
-      body: Container(
-        margin: const EdgeInsets.all(12),
-        child: const Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Super Liked Me',style: TextStyle(
-              fontSize: 22.5,
-              fontFamily: FF.raleway,
-              fontWeight: FontWeight.bold,
-            ),),
-            TextField(
-              autocorrect: true,
-              decoration: InputDecoration(
-                hintText: 'Search Friends'
+    return GestureDetector(
+      onTap: () => Get.focusScope!.unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 10),
+              child: Text(
+                'SELECT',
+                style: TextStyle(
+                    letterSpacing: 1.5,
+                    fontSize: 13,
+                    color: Colors.deepOrange,
+                    fontWeight: FontWeight.bold),
               ),
             )
           ],
+        ),
+        body: Container(
+          margin: const EdgeInsets.all(12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Super Liked Me',
+                style: TextStyle(
+                  fontSize: 22.5,
+                  fontFamily: FF.raleway,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Material(
+                elevation: 8.0,
+                shadowColor: Colors.black54,
+                borderRadius: BorderRadius.circular(5),
+                child: TextField(
+                  autocorrect: true,
+                  onChanged: (value) {
+                    controller.runFilter(value);
+                  },
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.grey,
+                      ),
+                      contentPadding: EdgeInsets.symmetric(vertical: 10),
+                      hintText: 'Search Friends',
+                      border: InputBorder.none),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                children: [
+                  const Text('Super Likes'),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    decoration: const BoxDecoration(
+                        color: Colors.deepOrange, shape: BoxShape.circle),
+                    alignment: Alignment.center,
+                    child: Padding(
+                      padding: const EdgeInsets.all(3.0),
+                      child: Text(
+                        '${controller.personList.length}',
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Obx(() => Flexible(
+                    child: controller.personList.isEmpty
+                        ? const Center(
+                            child: Text(
+                              'No Data Found',
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          )
+                        : ListView.builder(
+                            itemCount: controller.personList.length,
+                            itemBuilder: (context, index) {
+                              return Container(
+                                margin: const EdgeInsets.all(5),
+                                child: Material(
+                                  elevation: 8.0,
+                                  shadowColor: Colors.black54,
+                                  borderRadius: BorderRadius.circular(5),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      children: [
+                                        Container(
+                                          width: 60,
+                                          height: 60,
+                                          decoration: BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              image: DecorationImage(
+                                                  image: AssetImage(
+                                                    controller.personList[index]
+                                                        .image!,
+                                                  ),
+                                                  fit: BoxFit.cover)),
+                                        ),
+                                        const SizedBox(
+                                          width: 10,
+                                        ),
+                                        Expanded(
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(controller
+                                                  .personList[index].name!),
+                                              Row(
+                                                children: [
+                                                  const Icon(
+                                                    Icons.location_on_sharp,
+                                                    size: 12,
+                                                    color: Colors.grey,
+                                                  ),
+                                                  Text(controller
+                                                      .personList[index]
+                                                      .address!),
+                                                ],
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                        Column(
+                                          children: [
+                                            Text(
+                                              controller.personList[index]
+                                                      .isOnline!
+                                                  ? 'ONLINE'
+                                                  : 'OFFLINE',
+                                              style: TextStyle(
+                                                  letterSpacing: 1.5,
+                                                  fontSize: 10,
+                                                  fontWeight: FontWeight.bold,
+                                                  color: controller
+                                                          .personList[index]
+                                                          .isOnline!
+                                                      ? Colors.green
+                                                      : Colors.red),
+                                            ),
+                                            Icon(
+                                              controller.personList[index]
+                                                      .isSaved!
+                                                  ? Icons.star
+                                                  : Icons.star_outline,
+                                              color: controller
+                                                      .personList[index]
+                                                      .isSaved!
+                                                  ? Colors.blue
+                                                  : Colors.grey,
+                                            )
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                  ))
+            ],
+          ),
         ),
       ),
     );
